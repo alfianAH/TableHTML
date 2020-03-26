c3ae49db5743cd2a23c0e8ef5a5dc719940f6c4b
Alfian Hamdani <alfian.hamdani04@gmail.com>
2020-03-26 22:37:13 +0800

    Implement OOP to setData from form

--- Javascript OOP_Registrasi Mahasiswa/mhs.js
+++ Javascript OOP_Registrasi Mahasiswa/mhs.js
@@ -6,3 +6,37 @@ Minimal ada:
 3. fungsi tampilkanData()
 4. fungsi hapusData()
 */
+
+function Data(nama, nim, prodi, email){
+    this.nama = nama;
+    this.nim = nim;
+    this.prodi = prodi;
+    this.email = email;
+
+    this.getNama = function(){
+        return this.nama;
+    }
+
+    this.getNim = function(){
+        return this.nim;
+    }
+
+    this.getProdi = function(){
+        return this.prodi;
+    }
+
+    this.getEmail = function(){
+        return this.email;
+    }
+}
+
+function setData(){
+    var nama = document.forms["registrasiMhs"]["nama"].value;
+    var nim = document.forms["registrasiMhs"]["nim"].value;
+    var prodi = document.forms["registrasiMhs"]["prodi"].value;
+    var email = document.forms["registrasiMhs"]["email"].value;
+
+    var data = new Data(nama, nim, prodi, email);
+    
+    return data;
+}
