2d136e175dc5190e828918224cabe5f62906d472
Alfian Hamdani <alfian.hamdani04@gmail.com>
2020-03-31 15:27:05 +0800

    Improve email restriction in validateForm

--- Javascript OOP_Registrasi Mahasiswa/mhs.js
+++ Javascript OOP_Registrasi Mahasiswa/mhs.js
@@ -14,6 +14,7 @@ if(!(window.indexedDB)){
     alert("This browser doesn't support IndexedDB");
 }
 
+// Deklarasi variabel
 var request, 
     db, 
     transaction, 
@@ -21,9 +22,12 @@ var request,
     index;
 
 const dataMahasiswa = [];
+
+// Panggil init() saat pertama load web
 init();
 
 class Data {
+    // Constructor
     constructor(nama, nim, prodi, email) {
         this.nama = nama;
         this.nim = nim;
@@ -32,7 +36,6 @@ class Data {
     }
 
     // Getter 
-    // GET MAHASISWA PADA INDEX ???
     getNama = function () {
         return this.nama;
     };
@@ -47,27 +50,66 @@ class Data {
     };
 }
 
+// init: untuk menampilkan data pada saat web di load
+function init(){
+    // Buka database
+    request = window.indexedDB.open("database", 1); 
+
+    request.onupgradeneeded = function(event){
+        var db = request.result,
+            store = db.createObjectStore("dataMahasiswa"),
+            index = store.createIndex("nama", "nama", {unique: false});
+    };
+
+    request.onerror = function(event){
+        console.log("Error: " + event.target.errorCode);
+    };
+
+    request.onsuccess = function(event){
+        db = request.result;
+        transaction = db.transaction("dataMahasiswa", "readwrite");
+        store = transaction.objectStore("dataMahasiswa");
+        // dapatkan semua isi database
+        var dbMahasiswa = store.getAll();
+
+        dbMahasiswa.onsuccess = function(event){
+            header(); // tampilkan header
+            // Tampilkan semua isi database
+            for(var i=0; i<dbMahasiswa.result.length; i++){
+                tampilkanData(i, dbMahasiswa.result[i].nama, dbMahasiswa.result[i].nim, dbMahasiswa.result[i].prodi, dbMahasiswa.result[i].email);
+            }
+        }
+    };
+}
+
+// setData: untuk mengambil value dari input
 function setData(){
+    // Ambil value input user
     var nama = document.forms["registrasiMhs"]["nama"].value;
     var nim = document.forms["registrasiMhs"]["nim"].value;
     var prodi = document.forms["registrasiMhs"]["prodi"].value;
     var email = document.forms["registrasiMhs"]["email"].value;
 
+    // Cek apakah input user valid. Jika ya, maka ...
     if(validateForm(nama, nim, prodi, email)){
         var data = new Data(nama, nim, prodi, email);
-        dataMahasiswa.push({nama: nama, nim: nim, prodi: prodi, email: email});
+        // Masukkan data ke array dalam bentuk dictionary
+        dataMahasiswa.push({nama: data.getNama(), nim: data.getNim(), prodi: data.getProdi(), email: data.getEmail()});
         console.log(dataMahasiswa);
+        // Tambahkan data ke database
         addData();
     }
 }
 
+// addData: menambahkan data ke database
 function addData() {
+    // Buka database
     request = window.indexedDB.open("database", 1); 
         // db, 
         // transaction, 
         // store, 
         // index;
-        
+    
     request.onupgradeneeded = function(event){
         var db = request.result,
             store = db.createObjectStore("dataMahasiswa"),
@@ -89,12 +131,16 @@ function addData() {
             console.log("ERROR: " + event.target.errorCode);
         };
 
+        // Masukkan data ke database
         store.put(dataMahasiswa[dataMahasiswa.length - 1]);
+        // Dapatkan isi seluruh database
         var dbMahasiswa = store.getAll();
         
         dbMahasiswa.onsuccess = function(){
+            // Munculkan header
             header();
             console.log(dbMahasiswa.result);
+            // Tampilkan seluruh data dalam database
             for(var i=0; i<dbMahasiswa.result.length; i++){
                 tampilkanData(i, dbMahasiswa.result[i].nama, dbMahasiswa.result[i].nim, dbMahasiswa.result[i].prodi, dbMahasiswa.result[i].email);
             }
@@ -102,61 +148,42 @@ function addData() {
     };
 }
 
+// hapusData: untuk menghapus data berdasarkan index
 function hapusData(index){
     db = request.result;
     transaction = db.transaction("dataMahasiswa", "readwrite");
     store = transaction.objectStore("dataMahasiswa");
     // var delDb;
+    // Dapatkan semua isi database
     var dbMahasiswa = store.getAll();
 
     dbMahasiswa.onsuccess = function(event){
+        // Hapus isi database pada index yang ditentukan
         delDb = store.delete(dbMahasiswa.result[index].nim);
         alert(dbMahasiswa.result[index].nim + " sudah dihapus");
-        location.reload();
-        header();
+        location.reload(); // Reload page
+        header(); // Munculkan header
+        // Tampilkan semua isi database
         for(var i=0; i<dbMahasiswa.result.length; i++){
             tampilkanData(i, dbMahasiswa.result[i].nama, dbMahasiswa.result[i].nim, dbMahasiswa.result[i].prodi, dbMahasiswa.result[i].email);
         }
     }
 }
 
-function init(){
-    request = window.indexedDB.open("database", 1); 
-
-    request.onupgradeneeded = function(event){
-        var db = request.result,
-            store = db.createObjectStore("dataMahasiswa"),
-            index = store.createIndex("nama", "nama", {unique: false});
-    };
-
-    request.onerror = function(event){
-        console.log("Error: " + event.target.errorCode);
-    };
-
-    request.onsuccess = function(event){
-        db = request.result;
-        transaction = db.transaction("dataMahasiswa", "readwrite");
-        store = transaction.objectStore("dataMahasiswa");
-        var dbMahasiswa = store.getAll();
-
-        dbMahasiswa.onsuccess = function(event){
-            header();
-            for(var i=0; i<dbMahasiswa.result.length; i++){
-                tampilkanData(i, dbMahasiswa.result[i].nama, dbMahasiswa.result[i].nim, dbMahasiswa.result[i].prodi, dbMahasiswa.result[i].email);
-            }
-        }
-    };
-}
-
+// header: untuk menampilkan header tabel
 function header(){
+    // dapatkan id hasilTabel
     var hasilTabel = document.getElementById("hasilTabel");
+    // Kosongkan
     hasilTabel.innerHTML = "";
+    // Buat elemen tr dan th
     var trHeader = document.createElement("tr"),
         thNama = document.createElement("th"),
         thNim = document.createElement("th"),
         thProdi = document.createElement("th"),
         thEmail = document.createElement("th");
 
+    // Masukkan teks pada th lalu masukkan ke dalam tr
     thNama.textContent = "Nama";
     trHeader.appendChild(thNama);
     thNim.textContent = "Nim";
@@ -166,12 +193,16 @@ function header(){
     thEmail.textContent = "Email";
     trHeader.appendChild(thEmail);
 
+    // Masukkan tr ke dalam tabel
     hasilTabel.appendChild(trHeader);
 }
 
+// tampilkanData: untuk menampilkan data
 function tampilkanData(index, nama, nim, prodi, email){
+    // Dapatkan id hasilTabel
     var hasilTabel = document.getElementById("hasilTabel");
     
+    // Buat elemen tr td dan input
     var trData = document.createElement("tr"),
         tdNama = document.createElement("td"),
         tdNim = document.createElement("td"),
@@ -180,15 +211,16 @@ function tampilkanData(index, nama, nim, prodi, email){
         tdHapus = document.createElement("td"),
         hapusBtn = document.createElement("input");
     
-    tdHapus.className = "hapus";
-    hapusBtn.type = "button"
-    hapusBtn.id = "index";
-    // hapusBtn.onclick = "hapusData(index)";
+    tdHapus.className = "hapus"; // td untuk hapus diberi class hapus
+    hapusBtn.type = "button" // jenis input: button
+    hapusBtn.id = "index"; // id untuk button
+    // Tambahkan event click pada button
     hapusBtn.addEventListener("click", function(){
         hapusData(index);
     });
-    hapusBtn.value = "Hapus";
+    hapusBtn.value = "Hapus"; // Teks button
 
+    // Masukkan isi ke dalam td dan masukkan ke dalam tr
     tdNama.textContent = nama;
     trData.appendChild(tdNama);
     tdNim.textContent = nim;
@@ -200,14 +232,24 @@ function tampilkanData(index, nama, nim, prodi, email){
     tdHapus.appendChild(hapusBtn);
     trData.appendChild(tdHapus);
     
+    // Masukkan tr ke dalam tabel
     hasilTabel.appendChild(trData);
 }
 
+// validateForm: mengecek apakah data yang dimasukkan sudah betul dan tidak kosong
 function validateForm(nama, nim, prodi, email){
+    // Jika kosong, maka alert dan masukkan lagi
     if(nama == "" || nim == "" || prodi == "" || email == ""){
         alert("Anda harus melengkapi semua data.");
         return false;
-    } else{
+    } 
+    // Jika email tidak memiliki @, maka alert dan masukkan lagi
+    else if(email.search("@") == -1){
+        alert("Masukkan email yang benar dengan menggunakan @");
+        return false;
+    } 
+    // Selain itu benar.
+    else{
         return true;
     }
 }
